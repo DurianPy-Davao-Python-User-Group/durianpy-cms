f01f93143953255e881d1cf420e1a2fb03d71d76
TxcWb <jpnavares@up.edu.ph>
2026-08-04 16:34:34 +0800

    refactor(carousel): use global constants and clean up access control

--- src/globals/durianpy-website/Carousel.ts
+++ src/globals/durianpy-website/Carousel.ts
@@ -2,18 +2,19 @@ import { anyAdmin } from '@/access/anyAdmin'
 import { checkCollectionAccess } from '@/access/checkCollectionAccess'
 import { AccessType } from '@/constants/accessTypes'
 import { getCollectionGroupLabel } from '@/constants/collections'
+import { GLOBALS, GLOBAL_LABELS } from '@/constants/globals'
 import type { User } from '@/payload-types'
 import type { AccessArgs, GlobalConfig } from 'payload'
 
 const checkCarouselAccess = (accessType?: AccessType) => (access: AccessArgs) =>
-  checkCollectionAccess(access, 'carousel', accessType)
+  checkCollectionAccess(access, GLOBALS.DURIANPY_WEBSITE_CAROUSEL, accessType)
 
 const adminAccess = (access: AccessArgs<User>) =>
   Boolean(checkCarouselAccess('admin')(access) && anyAdmin(access))
 
 export const Carousel: GlobalConfig = {
-  slug: 'carousel',
-  label: 'Carousel',
+  slug: GLOBALS.DURIANPY_WEBSITE_CAROUSEL,
+  label: GLOBAL_LABELS[GLOBALS.DURIANPY_WEBSITE_CAROUSEL].singular,
   access: {
     read: (access) => {
       if (adminAccess(access)) {
