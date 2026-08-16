5b8ef1b7cedab263c8f94e335a31e612711f8622
Shekainah Bag-ao <bagaoshekainah@gmail.com>
2026-08-16 14:00:35 +0000

    feat(cms): add optional description field

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -424,6 +424,7 @@ export interface DurianpyWebsitePartner {
   name: string;
   logo: string | Media;
   websiteUrl?: string | null;
+  description?: string | null;
   updatedAt: string;
   createdAt: string;
   _status?: ('draft' | 'published') | null;
@@ -1028,6 +1029,7 @@ export interface DurianpyWebsitePartnersSelect<T extends boolean = true> {
   name?: T;
   logo?: T;
   websiteUrl?: T;
+  description?: T;
   updatedAt?: T;
   createdAt?: T;
   _status?: T;
